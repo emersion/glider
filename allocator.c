dc4fd172bc1ecf590251432fa5b97278c470f3d9
Simon Ser <contact@emersion.fr>
2019-11-28 15:45:41 +0100

    Add glider_buffer

--- allocator.c
+++ allocator.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wlr/util/log.h>
 #include "allocator.h"
 
 struct glider_allocator *glider_gbm_allocator_create(int fd) {
@@ -19,4 +22,81 @@ struct glider_allocator *glider_gbm_allocator_create(int fd) {
 void glider_allocator_destroy(struct glider_allocator *alloc) {
 	gbm_device_destroy(alloc->gbm_device);
 	free(alloc);
+}
+
+struct glider_buffer *glider_allocator_create_buffer(
+		struct glider_allocator *alloc, int width, int height, uint32_t format,
+		const uint64_t *modifiers, size_t modifiers_len) {
+	struct gbm_bo *bo = gbm_bo_create(alloc->gbm_device, width, height, format,
+		GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING);
+	if (bo == NULL) {
+		return NULL;
+	}
+
+	struct glider_buffer *buffer = calloc(1, sizeof(*buffer));
+	if (buffer == NULL) {
+		gbm_bo_destroy(bo);
+		return NULL;
+	}
+	buffer->gbm_bo = bo;
+	return buffer;
+}
+
+void glider_buffer_destroy(struct glider_buffer *buffer) {
+	if (buffer->dmabuf_attribs.n_planes > 0) {
+		wlr_dmabuf_attributes_finish(&buffer->dmabuf_attribs);
+	}
+	gbm_bo_destroy(buffer->gbm_bo);
+	free(buffer);
+}
+
+static bool buffer_get_dmabuf(struct glider_buffer *buffer,
+		struct wlr_dmabuf_attributes *attribs) {
+	memset(attribs, 0, sizeof(struct wlr_dmabuf_attributes));
+
+	struct gbm_bo *bo = buffer->gbm_bo;
+
+	attribs->n_planes = gbm_bo_get_plane_count(bo);
+	if (attribs->n_planes > WLR_DMABUF_MAX_PLANES) {
+		wlr_log(WLR_ERROR, "GBM BO contains too many planes (%d)",
+			attribs->n_planes);
+		return false;
+	}
+
+	attribs->width = gbm_bo_get_width(bo);
+	attribs->height = gbm_bo_get_height(bo);
+	attribs->format = gbm_bo_get_format(bo);
+	attribs->modifier = gbm_bo_get_modifier(bo);
+
+	int i;
+	for (i = 0; i < attribs->n_planes; ++i) {
+		attribs->offset[i] = gbm_bo_get_offset(bo, i);
+		attribs->stride[i] = gbm_bo_get_stride_for_plane(bo, i);
+		attribs->fd[i] = gbm_bo_get_fd(bo);
+		if (attribs->fd[i] < 0) {
+			wlr_log(WLR_ERROR, "gbm_bo_get_fd failed");
+			goto error_fd;
+		}
+	}
+
+	return true;
+
+error_fd:
+	for (int j = 0; j < i; ++j) {
+		close(attribs->fd[j]);
+	}
+	return false;
+}
+
+bool glider_buffer_get_dmabuf(struct glider_buffer *buffer,
+		struct wlr_dmabuf_attributes *attribs) {
+	if (buffer->dmabuf_attribs.n_planes == 0) {
+		if (!buffer_get_dmabuf(buffer, &buffer->dmabuf_attribs)) {
+			return false;
+		}
+	}
+
+	memcpy(attribs, &buffer->dmabuf_attribs,
+		sizeof(struct wlr_dmabuf_attributes));
+	return true;
 }
--- include/allocator.h
+++ include/allocator.h
@@ -2,13 +2,29 @@
 #define GLIDER_ALLOCATOR_H
 
 #include <gbm.h>
+#include <stdbool.h>
+#include <wlr/render/dmabuf.h>
+
+// TODO: turn this into an interface
+struct glider_buffer {
+	struct gbm_bo *gbm_bo;
+	struct wlr_dmabuf_attributes dmabuf_attribs;
+};
 
 // TODO: turn this into an interface
 struct glider_allocator {
 	struct gbm_device *gbm_device;
 };
 
-struct glider_allocator *glider_gbm_allocator_create(int fd);
 void glider_allocator_destroy(struct glider_allocator *alloc);
+struct glider_buffer *glider_allocator_create_buffer(
+	struct glider_allocator *alloc, int width, int height, uint32_t format,
+	const uint64_t *modifiers, size_t modifiers_len);
+
+void glider_buffer_destroy(struct glider_buffer *buffer);
+bool glider_buffer_get_dmabuf(struct glider_buffer *buffer,
+	struct wlr_dmabuf_attributes *attribs);
+
+struct glider_allocator *glider_gbm_allocator_create(int render_fd);
 
 #endif
